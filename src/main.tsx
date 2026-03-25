2dcc13e74659acd0072eba2531188ecdff42c920
Mohamed Nadeem <mmohamednadeem@gmail.com>
2026-03-25 14:04:05 +0300

    last hope

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+/// <reference types="vite/client" />
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
